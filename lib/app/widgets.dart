dfbc7b2e5dc3b73ae6fe17b06796a5f49ab8d270
Batjimin <147qqa@gmail.com>
2024-07-20 16:21:16 +0900

    last build..

--- lib/app/widgets.dart
+++ lib/app/widgets.dart
@@ -2,7 +2,10 @@ import 'package:flutter/material.dart';
 import 'package:tuple/tuple.dart';
 
 import '../../widget_book/hello_world_page_lego/_/_.dart' as hello_world_page_lego;
+import '../../widget_book/batjimin_intro_page_lego/_/_.dart' as mypage_page_lego;
 
 final List<Tuple2<String, Widget>> widgets = [
-  Tuple2('hello_world_page_lego', hello_world_page_lego.NewView())
+  Tuple2('hello_world_page_lego', hello_world_page_lego.NewView()),
+  Tuple2('myoage', mypage_page_lego.NewView()),
+
 ];
