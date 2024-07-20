6cddd042e03f961c64f01cdb60b6af594a862a3d
Batjimin <147qqa@gmail.com>
2024-07-20 15:27:29 +0900

    build web

--- lib/config.dart
+++ lib/config.dart
@@ -1,2 +1,2 @@
-String userName = 'June';
-String githubUrl = 'https://github.com/melodysdreamj/my_widget_book';
+String userName = 'Batjimin';
+String githubUrl = 'https://github.com/Batjimin/Batjimin_widget';
